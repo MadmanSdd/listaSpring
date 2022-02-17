b58350eff29a4f11ed06d381d5dff4bacd3bb67c
Diego <44852253+MadmanSdd@users.noreply.github.com>
2022-02-17 11:10:38 -0300

    Update PedidoController.java
    alterando cross

--- src/main/java/github/com/listaspring/rest/PedidoController.java
+++ src/main/java/github/com/listaspring/rest/PedidoController.java
@@ -21,7 +21,7 @@ import github.com.listaspring.repository.IPedido_Repository;
 
 @RestController
 @RequestMapping("/api/pedidos")
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("https://madmansdd.github.io/")
 public class PedidoController {
 
 	@Autowired
